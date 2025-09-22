a395571bea1ff3c3140e9c56897e382490957ee3
CF Wang <chingfeng.wang@gmail.com>
2025-09-22 16:41:37 +0800

    1. after/ftplugin

---
+++ lua/custom/plugins/catppuccin.lua
@@ -0,0 +1,28 @@
+if false then
+  return {}
+end
+return {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
+  config = function()
+    require('catppuccin').setup {
+      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { 'italic' }, -- Change the style of comments
+        conditionals = {},
+        loops = {},
+        functions = {},
+        keywords = { 'bold' },
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = { 'italic' },
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
+      },
+    }
+    vim.cmd.colorscheme 'catppuccin'
+  end,
+}
